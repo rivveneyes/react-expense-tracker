d97f0c497ddd3387b1cb3a548b26300633c7a039
rivveneyes <garciajoshua907@yahoo.com>
2021-03-30 16:06:08 -0700

    inital start to react project with bootstrap working

---
+++ src/formfield.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+    state = {
+        
+    };
+    render() {
+   
+  
+  
+      return (
+    <form>
+        <input type="text"/>
+
+    </form>
+      );
+    }
+  }
+  export default Form
